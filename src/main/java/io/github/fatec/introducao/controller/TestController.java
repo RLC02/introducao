4fb187b868217d35a5f448a80f5d2d8aa5a75454
RicardoLuquettiCodo <codor417@gmail.com>
2026-03-04 10:26:00 -0300

    implementação // POST // PUT // DELETE // GET

--- src/main/java/io/github/fatec/introducao/controller/TestController.java
+++ src/main/java/io/github/fatec/introducao/controller/TestController.java
@@ -1,17 +1,59 @@
 package io.github.fatec.introducao.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.UUID;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 @RestController
+@RequestMapping("/pessoa")
 public class TestController {
 
-    @GetMapping(value = "teste")
-    public String teste() {
-        String id = UUID.randomUUID().toString();
-        return id;
+    private List<Pessoa> pessoas = new ArrayList<>();
+    private Long contador = 1L;
+
+    static class Pessoa {
+        public Long id;
+        public String nome;
+        public String telefone;
+        public String endereco;
     }
 
+    // POST
+    @PostMapping
+    public Pessoa criar(@RequestBody Pessoa p) {
+        p.id = contador++;
+        pessoas.add(p);
+        return p;
+    }
+
+    // PUT
+    @PutMapping
+    public Pessoa atualizar(@RequestBody Pessoa p) {
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa.id.equals(p.id)) {
+                pessoa.nome = p.nome;
+                pessoa.telefone = p.telefone;
+                pessoa.endereco = p.endereco;
+                return pessoa;
+            }
+        }
+        return null;
+    }
+
+    // DELETE
+    @DeleteMapping
+    public String deletar(@RequestBody Pessoa p) {
+        for (Pessoa pessoa : pessoas) {
+            if (pessoa.id.equals(p.id)) {
+                pessoas.remove(pessoa);
+                return "Usuario:" + p.id + ".";
+            }
+        }
+        return "Usuario nao encontrado";
+    }
+
+    // GET
+    @GetMapping
+    public List<Pessoa> listar() {
+        return pessoas;
+    }
 }
